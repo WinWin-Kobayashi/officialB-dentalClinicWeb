51196572a2a1bb4d5b571b50881e24a618fda663
EdisonP <133362311+edions@users.noreply.github.com>
2023-11-07 20:39:04 +0800

    Display real data from database on appointment requests

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -1,3 +1,5 @@
+<?php include('connection.php'); ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,77 +23,56 @@
         <div class="appointment-requests-container">
             <h3>Appointment Requests</h3>
             <div class="appointment-req-table">
-                <table class="table">
-                    <thead class="thead">
-                        <tr>
-                            <th>Id</th>
-                            <th scope="col">Date Created</th>
-                            <th scope="col">Appointment Date</th>
-                            <th scope="col">Appointment Time</th>
-                            <th scope="col">Service</th>
-                            <th scope="col">Status</th>
-                            <th scope="col">Actions</th>
-                        </tr>
-                    </thead>
+            <?php
 
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>2023-10-01</td>
-                            <td>2023-11-15</td>
-                            <td>10:00 AM</td>
-                            <td>Dental Checkup</td>
-                            <td>Accepted</td>
-                            <td>
-                                <button>Cancel</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>2023-10-02</td>
-                            <td>2023-11-16</td>
-                            <td>2:30 PM</td>
-                            <td>Tooth Cleaning</td>
-                            <td>Pending</td>
-                            <td>
-                                <button>Cancel</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            <td>2023-10-03</td>
-                            <td>2023-11-17</td>
-                            <td>4:00 PM</td>
-                            <td>Tooth Extraction</td>
-                            <td>Cancelled</td>
-                            <td>
-                                <button>Cancel</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>4</td>
-                            <td>2023-10-04</td>
-                            <td>2023-11-18</td>
-                            <td>1:15 PM</td>
-                            <td>Root Canal</td>
-                            <td>Accepted</td>
-                            <td>
-                                <button>Cancel</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>5</td>
-                            <td>2023-10-05</td>
-                            <td>2023-11-19</td>
-                            <td>11:30 AM</td>
-                            <td>Dental Implant</td>
-                            <td>Pending</td>
-                            <td>
-                                <button>Cancel</button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+            // Check for connection 
+            if ($conn) {
+
+                $query = "SELECT * FROM appointments";
+                $result = mysqli_query($conn, $query);
+                if ($result) {
+                    echo '<table class="table">
+                        <thead class="thead">
+                            <tr>
+                                <th>Id</th>
+                                <th scope="col">Date Created</th>
+                                <th scope="col">Appointment Date</th>
+                                <th scope="col">Appointment Time</th>
+                                <th scope="col">Service</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Actions</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>';
+
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            echo '<tr>
+                                <td>' . $row['id'] . '</td>
+                                <td>' . $row['date_created'] . '</td>
+                                <td>' . $row['date'] . '</td>
+                                <td>' . date('h:i A', strtotime($row['time'])) . '</td>
+                                <td>' . $row['service'] . '</td>
+                                <td>' . $row['status'] . '</td>
+                                <td>
+                                    <button>Confirm</button>
+                                    <button>Reschedule</button>
+                                    <button>Cancel</button>
+                                </td>
+                            </tr>';
+                        }
+
+                        echo '</tbody></table>';
+                        } else {
+                            echo 'Error: ' . mysqli_error($conn);
+                        }
+                } else {
+                    echo 'Database connection failed: ' . mysqli_connect_error();
+                    }
+                    // close connetions
+                    mysqli_close($conn);
+                ?>
+
             </div>
         </div>
     </section>
