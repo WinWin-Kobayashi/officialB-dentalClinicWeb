7d3d07d2b172266f320fd32be1ef0435fca0410e
EdisonP <133362311+edions@users.noreply.github.com>
2023-11-08 02:06:37 +0800

    add admin-patients.css

--- admin-patients.php
+++ admin-patients.php
@@ -9,53 +9,52 @@
 </head>
 <body>
     <?php include('admin-header.php'); ?>
-    <table border="1">
-    <tr>
-        <th>ID</th>
-        <th>First Name</th>
-        <th>Last Name</th>
-        <th>Birthdate</th>
-        <th>Gender</th>
-        <th>Address</th>
-        <th>Contact Number</th>
-        <th>Active Gmail</th>
-        <th>Verified</th>
-        <th>Create Date</th>
-    </tr>
 
-    <?php
+        <div class="patients-container">
+            <table border="1">
+            <tr>
+                <th>ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Birthdate</th>
+                <th>Gender</th>
+                <th>Address</th>
+                <th>Contact Number</th>
+                <th>Active Gmail</th>
+                <th>Verified</th>
+                <th>Create Date</th>
+            </tr>
 
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+            <?php
 
-    // Query to fetch data from the patients_table1
-    $sql = "SELECT * FROM patients_table1";
-    $result = $conn->query($sql);
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
 
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            echo "<tr>";
-            echo "<td>" . $row["id"] . "</td>";
-            echo "<td>" . $row["first_name"] . "</td>";
-            echo "<td>" . $row["last_name"] . "</td>";
-            echo "<td>" . $row["birthdate"] . "</td>";
-            echo "<td>" . $row["gender"] . "</td>";
-            echo "<td>" . $row["address"] . "</td>";
-            echo "<td>" . $row["contact_number"] . "</td>";
-            echo "<td>" . $row["active_gmail"] . "</td>";
-            echo "<td>" . $row["verified"] . "</td>";
-            echo "<td>" . $row["create_date"] . "</td>";
-            echo "</tr>";
-        }
-    } else {
-        echo "No records found";
-    }
+            $sql = "SELECT * FROM patients_table1";
+            $result = $conn->query($sql);
 
-    // Close the database connection
-    $conn->close();
-    ?>
-</table>
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    echo "<tr>";
+                    echo "<td>" . $row["id"] . "</td>";
+                    echo "<td>" . $row["first_name"] . "</td>";
+                    echo "<td>" . $row["last_name"] . "</td>";
+                    echo "<td>" . $row["birthdate"] . "</td>";
+                    echo "<td>" . $row["gender"] . "</td>";
+                    echo "<td>" . $row["address"] . "</td>";
+                    echo "<td>" . $row["contact_number"] . "</td>";
+                    echo "<td>" . $row["active_gmail"] . "</td>";
+                    echo "<td>" . $row["verified"] . "</td>";
+                    echo "<td>" . $row["create_date"] . "</td>";
+                    echo "</tr>";
+                }
+            } else {
+                echo "No records found";
+            }
+            $conn->close();
+            ?>
+        </table>
+    </div>
 </body>
 </html>
