63da956197c43b4b3ecf52be4b76df2989af8435
EdisonP <133362311+edions@users.noreply.github.com>
2023-11-10 19:19:33 +0800

    Minor fix to sidebar

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -5,187 +5,180 @@
 <head>
     <?php include('globalHead.php'); ?>
     <link rel="stylesheet" href="admin-dashboard.css">
-    <link rel="stylesheet" href="indexStyle.css">
+    <!-- <link rel="stylesheet" href="indexStyle.css"> -->
 </head>
 <body>
 
     <?php include('admin-header.php');?>
 
-    <!-- for error on appointment status -->
-    <?php if (isset($_GET['error'])) { ?>
-        <script>
-            alert("<?php echo $_GET['error']; ?>");
-        </script>
-    <?php } ?>
-
-
-    <section id="home">
-        <div class="row">
-            <div class="confirmed-appointments-container">Confirmed Appointments
-            <div class="appointment-req-table">
-                    <?php
-
-                    // Check for connection 
-                    if ($conn) {
-
-                        $query = "SELECT * FROM appointments WHERE status = 'Accepted'";
-                        $result = mysqli_query($conn, $query);
-                        if ($result) {
-                            echo '<table class="table">
-                                <thead class="thead">
-                                    <tr>
-                                        <th>Id</th>
-                                        <th scope="col">Fullname</th>
-                                        <th scope="col">Date Created</th>
-                                        <!-- <th scope="col">Appointment Date</th> -->
-                                        <!-- <th scope="col">Appointment Time</th> -->
-                                        <th scope="col">Service</th>
-                                        <th scope="col">Status</th>
-                                    </tr>
-                                </thead>
-
-                                <tbody>';
-
-                                while ($row = mysqli_fetch_assoc($result)) {
-                                    echo '<tr>
-                                        <td>' . $row['id'] . '</td>
-                                        <td>' . $row['first_name'] . ' ' . $row['last_name'] . '</td>
-                                        <td>' . date('Y-m-d', strtotime($row['date_created'])) . '</td>
-                                        <!-- <td>' . $row['date'] . '</td> -->
-                                        <!-- <td>' . date('h:i A', strtotime($row['time'])) . '</td> -->
-                                        <td>' . $row['service'] . '</td>
-                                        <td>' . $row['status'] . '</td>
-                                    </tr>';
-                                }
-
-                                echo '</tbody></table>';
-                                } else {
-                                    echo 'Error: ' . mysqli_error($conn);
-                                }
-                        } else {
-                            echo 'Database connection failed: ' . mysqli_connect_error();
-                            }
-                            // close connetions
-                            //mysqli_close($conn);
-                        ?>
-                    </div>
-            </div>
-            <div class="numbers-container">
-                <div class="cancellations">Cancelled: 
+    <div class="dashboard-container">
+        <section id="home">
+            <div class="row">
+                <div class="confirmed-appointments-container">Confirmed Appointments
                 <div class="appointment-req-table">
-                    <?php
+                        <?php
 
-                    // Check for connection 
-                    if ($conn) {
+                        // Check for connection 
+                        if ($conn) {
 
-                        $query = "SELECT * FROM appointments WHERE status = 'Canceled'";
-                        $result = mysqli_query($conn, $query);
-                        if ($result) {
-                            echo '<table class="table">
-                                <thead class="thead">
-                                    <tr>
-                                        <th>Id</th>
-                                        <th scope="col">Fullname</th>
-                                        <th scope="col">Date Created</th>
-                                        <!-- <th scope="col">Appointment Date</th> -->
-                                        <!-- <th scope="col">Appointment Time</th> -->
-                                        <th scope="col">Service</th>
-                                        <th scope="col">Status</th>
-                                    </tr>
-                                </thead>
+                            $query = "SELECT * FROM appointments WHERE status = 'Accepted'";
+                            $result = mysqli_query($conn, $query);
+                            if ($result) {
+                                echo '<table class="table">
+                                    <thead class="thead">
+                                        <tr>
+                                            <th>Id</th>
+                                            <th scope="col">Fullname</th>
+                                            <th scope="col">Date Created</th>
+                                            <!-- <th scope="col">Appointment Date</th> -->
+                                            <!-- <th scope="col">Appointment Time</th> -->
+                                            <th scope="col">Service</th>
+                                            <th scope="col">Status</th>
+                                        </tr>
+                                    </thead>
 
-                                <tbody>';
+                                    <tbody>';
 
-                                while ($row = mysqli_fetch_assoc($result)) {
-                                    echo '<tr>
-                                        <td>' . $row['id'] . '</td>
-                                        <td>' . $row['first_name'] . ' ' . $row['last_name'] . '</td>
-                                        <td>' . date('Y-m-d', strtotime($row['date_created'])) . '</td>
-                                        <!-- <td>' . $row['date'] . '</td> -->
-                                        <!-- <td>' . date('h:i A', strtotime($row['time'])) . '</td> -->
-                                        <td>' . $row['service'] . '</td>
-                                        <td>' . $row['status'] . '</td>
-                                    </tr>';
+                                    while ($row = mysqli_fetch_assoc($result)) {
+                                        echo '<tr>
+                                            <td>' . $row['id'] . '</td>
+                                            <td>' . $row['first_name'] . ' ' . $row['last_name'] . '</td>
+                                            <td>' . date('Y-m-d', strtotime($row['date_created'])) . '</td>
+                                            <!-- <td>' . $row['date'] . '</td> -->
+                                            <!-- <td>' . date('h:i A', strtotime($row['time'])) . '</td> -->
+                                            <td>' . $row['service'] . '</td>
+                                            <td>' . $row['status'] . '</td>
+                                        </tr>';
+                                    }
+
+                                    echo '</tbody></table>';
+                                    } else {
+                                        echo 'Error: ' . mysqli_error($conn);
+                                    }
+                            } else {
+                                echo 'Database connection failed: ' . mysqli_connect_error();
                                 }
+                                // close connetions
+                                //mysqli_close($conn);
+                            ?>
+                        </div>
+                </div>
+                <div class="numbers-container">
+                    <div class="cancellations">Cancelled: 
+                    <div class="appointment-req-table">
+                        <?php
 
-                                echo '</tbody></table>';
-                                } else {
-                                    echo 'Error: ' . mysqli_error($conn);
+                        // Check for connection 
+                        if ($conn) {
+
+                            $query = "SELECT * FROM appointments WHERE status = 'Canceled'";
+                            $result = mysqli_query($conn, $query);
+                            if ($result) {
+                                echo '<table class="table">
+                                    <thead class="thead">
+                                        <tr>
+                                            <th>Id</th>
+                                            <th scope="col">Fullname</th>
+                                            <th scope="col">Date Created</th>
+                                            <!-- <th scope="col">Appointment Date</th> -->
+                                            <!-- <th scope="col">Appointment Time</th> -->
+                                            <th scope="col">Service</th>
+                                            <th scope="col">Status</th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody>';
+
+                                    while ($row = mysqli_fetch_assoc($result)) {
+                                        echo '<tr>
+                                            <td>' . $row['id'] . '</td>
+                                            <td>' . $row['first_name'] . ' ' . $row['last_name'] . '</td>
+                                            <td>' . date('Y-m-d', strtotime($row['date_created'])) . '</td>
+                                            <!-- <td>' . $row['date'] . '</td> -->
+                                            <!-- <td>' . date('h:i A', strtotime($row['time'])) . '</td> -->
+                                            <td>' . $row['service'] . '</td>
+                                            <td>' . $row['status'] . '</td>
+                                        </tr>';
+                                    }
+
+                                    echo '</tbody></table>';
+                                    } else {
+                                        echo 'Error: ' . mysqli_error($conn);
+                                    }
+                            } else {
+                                echo 'Database connection failed: ' . mysqli_connect_error();
                                 }
-                        } else {
-                            echo 'Database connection failed: ' . mysqli_connect_error();
-                            }
-                            // close connetions
-                            //mysqli_close($conn);
-                        ?>
+                                // close connetions
+                                //mysqli_close($conn);
+                            ?>
+                        </div>
+                    </div>
+                    <div class="accepted">Accepted:
                     </div>
                 </div>
-                <div class="accepted">Accepted:
+            </div>
+
+            <div class="appointment-requests-container">
+                <h3>Appointment Requests</h3>
+                <div class="appointment-req-table">
+                <?php
+
+                // Check for connection 
+                if ($conn) {
+
+                    $query = "SELECT * FROM appointments WHERE status = 'Pending'";
+                    $result = mysqli_query($conn, $query);
+                    if ($result) {
+                        echo '<table class="table">
+                            <thead class="thead">
+                                <tr>
+                                    <th>Id</th>
+                                    <th scope="col">Fullname</th>
+                                    <th scope="col">Date Created</th>
+                                    <th scope="col">Appointment Date</th>
+                                    <th scope="col">Appointment Time</th>
+                                    <th scope="col">Service</th>
+                                    <th scope="col">Status</th>
+                                    <th scope="col">Actions</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>';
+
+                            while ($row = mysqli_fetch_assoc($result)) {
+                                echo '<tr>
+                                    <td>' . $row['id'] . '</td>
+                                    <td>' . $row['first_name'] . ' ' . $row['last_name'] . '</td>
+                                    <td>' . date('Y-m-d', strtotime($row['date_created'])) . '</td>
+                                    <td>' . $row['date'] . '</td>
+                                    <td>' . date('h:i A', strtotime($row['time'])) . '</td>
+                                    <td>' . $row['service'] . '</td>
+                                    <td>' . $row['status'] . '</td>
+                                    <td>
+                                        <form method="post" action="lib/appointment-status.php">
+                                            <input type="hidden" name="appointmentId" value="' . $row['id'] . '">
+                                            <button type="submit" name="cancel">Cancel</button>
+                                            <button>Reschedule</button>
+                                            <button type="submit" name="accept">Accept</button>
+                                        </form>
+                                    </td>
+                                </tr>';
+                            }
+
+                            echo '</tbody></table>';
+                            } else {
+                                echo 'Error: ' . mysqli_error($conn);
+                            }
+                    } else {
+                        echo 'Database connection failed: ' . mysqli_connect_error();
+                        }
+                        // close connetions
+                        //mysqli_close($conn);
+                    ?>
+
                 </div>
             </div>
-        </div>
-
-        <div class="appointment-requests-container">
-            <h3>Appointment Requests</h3>
-            <div class="appointment-req-table">
-            <?php
-
-            // Check for connection 
-            if ($conn) {
-
-                $query = "SELECT * FROM appointments WHERE status = 'Pending'";
-                $result = mysqli_query($conn, $query);
-                if ($result) {
-                    echo '<table class="table">
-                        <thead class="thead">
-                            <tr>
-                                <th>Id</th>
-                                <th scope="col">Fullname</th>
-                                <th scope="col">Date Created</th>
-                                <th scope="col">Appointment Date</th>
-                                <th scope="col">Appointment Time</th>
-                                <th scope="col">Service</th>
-                                <th scope="col">Status</th>
-                                <th scope="col">Actions</th>
-                            </tr>
-                        </thead>
-
-                        <tbody>';
-
-                        while ($row = mysqli_fetch_assoc($result)) {
-                            echo '<tr>
-                                <td>' . $row['id'] . '</td>
-                                <td>' . $row['first_name'] . ' ' . $row['last_name'] . '</td>
-                                <td>' . date('Y-m-d', strtotime($row['date_created'])) . '</td>
-                                <td>' . $row['date'] . '</td>
-                                <td>' . date('h:i A', strtotime($row['time'])) . '</td>
-                                <td>' . $row['service'] . '</td>
-                                <td>' . $row['status'] . '</td>
-                                <td>
-                                    <form method="post" action="lib/appointment-status.php">
-                                        <input type="hidden" name="appointmentId" value="' . $row['id'] . '">
-                                        <button type="submit" name="cancel">Cancel</button>
-                                        <button>Reschedule</button>
-                                        <button type="submit" name="accept">Accept</button>
-                                    </form>
-                                </td>
-                            </tr>';
-                        }
-
-                        echo '</tbody></table>';
-                        } else {
-                            echo 'Error: ' . mysqli_error($conn);
-                        }
-                } else {
-                    echo 'Database connection failed: ' . mysqli_connect_error();
-                    }
-                    // close connetions
-                    //mysqli_close($conn);
-                ?>
-
-            </div>
-        </div>
-    </section>
-
+        </section>
+    </div>
 </body>
 </html>
--- admin-patients.php
+++ admin-patients.php
@@ -6,55 +6,58 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="admin-patients.css">
 </head>
 <body>
     <?php include('admin-header.php'); ?>
 
         <div class="patients-container">
-            <table border="1">
-            <tr>
-                <th>ID</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Birthdate</th>
-                <th>Gender</th>
-                <th>Address</th>
-                <th>Contact Number</th>
-                <th>Active Gmail</th>
-                <th>Verified</th>
-                <th>Create Date</th>
-            </tr>
+            <div class="appointment-details">
+                <table border="1">
+                <tr>
+                    <th>ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Birthdate</th>
+                    <th>Gender</th>
+                    <th>Address</th>
+                    <th>Contact Number</th>
+                    <th>Active Gmail</th>
+                    <th>Verified</th>
+                    <th>Create Date</th>
+                </tr>
 
-            <?php
+                <?php
 
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            }
-
-            $sql = "SELECT * FROM patients_table1";
-            $result = $conn->query($sql);
-
-            if ($result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
-                    echo "<tr>";
-                    echo "<td>" . $row["id"] . "</td>";
-                    echo "<td>" . $row["first_name"] . "</td>";
-                    echo "<td>" . $row["last_name"] . "</td>";
-                    echo "<td>" . $row["birthdate"] . "</td>";
-                    echo "<td>" . $row["gender"] . "</td>";
-                    echo "<td>" . $row["address"] . "</td>";
-                    echo "<td>" . $row["contact_number"] . "</td>";
-                    echo "<td>" . $row["active_gmail"] . "</td>";
-                    echo "<td>" . $row["verified"] . "</td>";
-                    echo "<td>" . $row["create_date"] . "</td>";
-                    echo "</tr>";
+                if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
                 }
-            } else {
-                echo "No records found";
-            }
-            $conn->close();
-            ?>
-        </table>
+
+                $sql = "SELECT * FROM patients_table1";
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<tr>";
+                        echo "<td>" . $row["id"] . "</td>";
+                        echo "<td>" . $row["first_name"] . "</td>";
+                        echo "<td>" . $row["last_name"] . "</td>";
+                        echo "<td>" . $row["birthdate"] . "</td>";
+                        echo "<td>" . $row["gender"] . "</td>";
+                        echo "<td>" . $row["address"] . "</td>";
+                        echo "<td>" . $row["contact_number"] . "</td>";
+                        echo "<td>" . $row["active_gmail"] . "</td>";
+                        echo "<td>" . $row["verified"] . "</td>";
+                        echo "<td>" . $row["create_date"] . "</td>";
+                        echo "</tr>";
+                    }
+                } else {
+                    echo "No records found";
+                }
+                $conn->close();
+                ?>
+            </table>
+        </div>
     </div>
 </body>
 </html>
