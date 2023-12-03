c68dab6b34956ce062719884f9be9b2580146f09
EdisonP <133362311+edions@users.noreply.github.com>
2023-12-04 03:06:51 +0800

    Refactor: Separate bar chart

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -156,6 +156,7 @@ if ($resultAccepted && $resultCancelled) {
     </div>
 
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="script/bar-chart.js" defer></script>
     <script>
     //Make the AJAX request
         function acceptAppointment(appointmentId) {
@@ -178,58 +179,6 @@ if ($resultAccepted && $resultCancelled) {
 
             window.location.href = url;
         }
-    </script>
-    <script>
-        // Mock Data
-        var data = {
-            labels: ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7'],
-            datasets: [
-                {
-                    label: 'Pending',
-                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                    borderColor: 'rgba(255, 99, 132, 1)',
-                    borderWidth: 1,
-                    data: [5, 8, 3, 2, 7, 4, 6]
-                },
-                {
-                    label: 'Accepted',
-                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                    borderColor: 'rgba(75, 192, 192, 1)',
-                    borderWidth: 1,
-                    data: [3, 6, 2, 4, 9, 5, 8]
-                },
-                {
-                    label: 'Cancelled',
-                    backgroundColor: 'rgba(255, 206, 86, 0.2)',
-                    borderColor: 'rgba(255, 206, 86, 1)',
-                    borderWidth: 1,
-                    data: [1, 4, 7, 3, 5, 2, 9]
-                }
-            ]
-        };
-
-        // Create Bar Chart
-        var ctx = document.getElementById('myBarChart').getContext('2d');
-        var myBarChart = new Chart(ctx, {
-            type: 'bar',
-            data: data,
-            options: {
-                scales: {
-                    x: {
-                        stacked: true,
-                        ticks: {
-                            color: '#531A62' // text color
-                        }
-                    },
-                    y: {
-                        stacked: true,
-                        ticks: {
-                            color: '#531A62' // text color
-                        }
-                    }
-                }
-            }
-        });
     </script>
 </body>
 </html>
---
+++ script/bar-chart.js
@@ -0,0 +1,50 @@
+ // Mock Data
+ var data = {
+    labels: ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7'],
+    datasets: [
+        {
+            label: 'Pending',
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1,
+            data: [5, 8, 3, 2, 7, 4, 6]
+        },
+        {
+            label: 'Accepted',
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1,
+            data: [3, 6, 2, 4, 9, 5, 8]
+        },
+        {
+            label: 'Cancelled',
+            backgroundColor: 'rgba(255, 206, 86, 0.2)',
+            borderColor: 'rgba(255, 206, 86, 1)',
+            borderWidth: 1,
+            data: [1, 4, 7, 3, 5, 2, 9]
+        }
+    ]
+};
+
+// Create Bar Chart
+var ctx = document.getElementById('myBarChart').getContext('2d');
+var myBarChart = new Chart(ctx, {
+    type: 'bar',
+    data: data,
+    options: {
+        scales: {
+            x: {
+                stacked: true,
+                ticks: {
+                    color: '#531A62' // text color
+                }
+            },
+            y: {
+                stacked: true,
+                ticks: {
+                    color: '#531A62' // text color
+                }
+            }
+        }
+    }
+});
