1923c7a6a97b672209f9fbb13400ccc0384e5612
EdisonP <133362311+edions@users.noreply.github.com>
2023-12-04 03:01:31 +0800

    Add bar chart

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -34,6 +34,7 @@ if ($resultAccepted && $resultCancelled) {
     <?php include('globalHead.php'); ?>
     <link rel="stylesheet" href="admin-dashboard.css">
     <!-- <link rel="stylesheet" href="indexStyle.css"> -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <style>
        body{
          background: var(--v-light-purple);
@@ -51,8 +52,10 @@ if ($resultAccepted && $resultCancelled) {
                 <!-- confirmed appointments box -->
                 <div class="confirmed-appointments-container">Confirmed Appointments
                     <div class="appointment-req-table">
-                        
+                        <div style="width: 80%; margin: auto;">
+                            <canvas id="myBarChart" width="400" height="150"></canvas>
                         </div>
+                    </div>
                 </div>
 
                 <!-- numbers box -->
@@ -175,6 +178,58 @@ if ($resultAccepted && $resultCancelled) {
 
             window.location.href = url;
         }
+    </script>
+    <script>
+        // Mock Data
+        var data = {
+            labels: ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7'],
+            datasets: [
+                {
+                    label: 'Pending',
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderColor: 'rgba(255, 99, 132, 1)',
+                    borderWidth: 1,
+                    data: [5, 8, 3, 2, 7, 4, 6]
+                },
+                {
+                    label: 'Accepted',
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 1,
+                    data: [3, 6, 2, 4, 9, 5, 8]
+                },
+                {
+                    label: 'Cancelled',
+                    backgroundColor: 'rgba(255, 206, 86, 0.2)',
+                    borderColor: 'rgba(255, 206, 86, 1)',
+                    borderWidth: 1,
+                    data: [1, 4, 7, 3, 5, 2, 9]
+                }
+            ]
+        };
+
+        // Create Bar Chart
+        var ctx = document.getElementById('myBarChart').getContext('2d');
+        var myBarChart = new Chart(ctx, {
+            type: 'bar',
+            data: data,
+            options: {
+                scales: {
+                    x: {
+                        stacked: true,
+                        ticks: {
+                            color: '#531A62' // text color
+                        }
+                    },
+                    y: {
+                        stacked: true,
+                        ticks: {
+                            color: '#531A62' // text color
+                        }
+                    }
+                }
+            }
+        });
     </script>
 </body>
 </html>
