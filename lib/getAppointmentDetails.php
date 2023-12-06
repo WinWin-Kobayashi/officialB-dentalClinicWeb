63bfa8dfaabe857aebb106445a06da88589aa092
EdisonP <133362311+edions@users.noreply.github.com>
2023-12-06 10:58:19 +0800

    Display patient on modal

---
+++ lib/getAppointmentDetails.php
@@ -0,0 +1,28 @@
+<?php
+$appointmentId = $_POST['appointmentId'];
+
+include '../connection.php';
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Use prepared statement to prevent SQL injection
+$query = "SELECT * FROM appointments WHERE id = ?";
+$stmt = $conn->prepare($query);
+$stmt->bind_param("i", $appointmentId); // "i" means int
+$stmt->execute();
+$result = $stmt->get_result();
+
+// Check if a row is returned
+if ($result->num_rows > 0) {
+    $appointmentDetails = $result->fetch_assoc();
+    echo json_encode($appointmentDetails);
+} else {
+    echo json_encode(array('error' => 'Appointment not found'));
+}
+
+$stmt->close();
+$conn->close();
+?>
--- modal/accept-modal.php
+++ modal/accept-modal.php
@@ -2,13 +2,20 @@
     <div>This is a modal</div>
     <form id="appointmentForm" method="POST">
         <input type="text" id="appointmentIdInput" name="appointmentId" readonly>
+        <br>
+        <input type="text" id="firstNameInput" name="firstName" placeholder="First Name" readonly>
+        <br>
+        <input type="text" id="lastNameInput" name="lastName" placeholder="Last Name" readonly>
+        <br>
+        <img id="screenshotImg" alt="Screenshot" style="max-width: 100%; max-height: 200px;">
+        <br>
         <button onclick="closeModal()" type="button">Close</button>
         <button type="submit" id="submitBtn">Submit</button>
     </form>
 </dialog>
 
 <script>
-    // AJAX Request
+    // AJAX Request to accpet the appoitment
     document.addEventListener('DOMContentLoaded', function () {
         document.getElementById('submitBtn').addEventListener('click', function (event) {
             event.preventDefault();
@@ -33,15 +40,41 @@
         });
     });
 
-    // Open/Close Modal
+    // Open/Close Modal + get patient data base on ID
     const modal = document.getElementById("modal");
 
-        function openModal(appointmentId) {
-            document.getElementById('appointmentIdInput').value = appointmentId;
-            document.getElementById('modal').showModal();
-        }
+    function openModal(appointmentId) {
+        document.getElementById('appointmentIdInput').value = appointmentId;
 
-        function closeModal() {
-            modal.close();
-        }
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', 'lib/getAppointmentDetails.php', true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    const appointmentDetails = JSON.parse(xhr.responseText);
+
+                    // Display details in textbox
+                    document.getElementById('firstNameInput').value = appointmentDetails.first_name;
+                    document.getElementById('lastNameInput').value = appointmentDetails.last_name;
+                    
+                    // Display screenshot
+                    const screenshotImg = document.getElementById('screenshotImg');
+                    const screenshotPath = 'screenshots/' + appointmentDetails.screenshot;
+                    screenshotImg.src = screenshotPath;
+                } else {
+                    console.error('Error fetching appointment details');
+                }
+            }
+        };
+
+        xhr.send('appointmentId=' + encodeURIComponent(appointmentId));
+
+        modal.showModal();
+    }
+
+    function closeModal() {
+        modal.close();
+    }
 </script>
