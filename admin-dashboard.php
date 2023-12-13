fa72dc26a49bfaa4f52c84afcefb1f077c25fa36
EdisonP <133362311+edions@users.noreply.github.com>
2023-12-13 21:16:50 +0800

    Improvements sidebar

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -44,113 +44,116 @@ if ($resultPending) {
 
     <?php include('admin-sidebar.php');?>
 
-    <div class="dashboard-container">
-        <section id="home">
-            <div class="row">
-                <!-- confirmed appointments box -->
-                <div class="confirmed-appointments-container">Confirmed Appointments
-                    <div class="appointment-req-table">
-                        <div style="width: 80%; margin: auto;">
-                            <canvas id="myBarChart" width="400" height="150"></canvas>
+    <section class="dashboard">
+        <div class="dashboard-container">
+            <section id="home">
+                <div class="row">
+                    <!-- confirmed appointments box -->
+                    <div class="confirmed-appointments-container">Confirmed Appointments
+                        <div class="appointment-req-table">
+                            <div style="width: 80%; margin: auto;">
+                                <canvas id="myBarChart" width="400" height="150"></canvas>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- numbers box -->
+                    <div class="numbers-container" style="display: flex; align-items: center; justify-content: center;">
+                        <div style="position: relative; margin-top: -6.5rem; height: 266px; width: 266px;" >
+                            <canvas id="appointmentChart" ></canvas>
                         </div>
                     </div>
                 </div>
 
-                <!-- numbers box -->
-                <div class="numbers-container" style="display: flex; align-items: center; justify-content: center;">
-                    <div style="position: relative; margin-top: -6.5rem; height: 266px; width: 266px;" >
-                        <canvas id="appointmentChart" ></canvas>
+                <!-- appointments req container -->
+                <div class="appointment-requests-container">
+                
+
+                    <div class="ap-req-con-row">
+                        <h3>Appointment Requests ( <?php echo $pendingCount; ?> )</h3>
+
+                        <div class="selection">
+                            <form method="get" action="admin-dashboard.php" >
+                                <!-- Dropdown for filtering by status -->
+                                <label for="statusFilter">Filter by Status:</label>
+                                <select id="statusFilter" name="status" onchange="this.form.submit()">
+                                    <option value="Pending" <?php echo (isset($_GET['status']) && $_GET['status'] == 'Pending') ? 'selected' : ''; ?>>Pending</option>
+                                    <option value="Cancelled" <?php echo (isset($_GET['status']) && $_GET['status'] == 'Cancelled') ? 'selected' : ''; ?>>Cancelled</option>
+                                    <option value="Accepted" <?php echo (isset($_GET['status']) && $_GET['status'] == 'Accepted') ? 'selected' : ''; ?>>Accepted</option>
+                                </select>
+                            </form>
+                        </div>
+                
+
                     </div>
-                </div>
-            </div>
+                
 
-            <!-- appointments req container -->
-            <div class="appointment-requests-container">
-               
+                    <div class="appointment-req-table">
+                        <?php
+                        // Check for connection 
+                        if ($conn) {
+                            // Default status filter value
+                            $statusFilter = 'Pending';
 
-                <div class="ap-req-con-row">
-                    <h3>Appointment Requests ( <?php echo $pendingCount; ?> )</h3>
-
-                    <div class="selection">
-                        <form method="get" action="admin-dashboard.php" >
-                            <!-- Dropdown for filtering by status -->
-                            <label for="statusFilter">Filter by Status:</label>
-                            <select id="statusFilter" name="status" onchange="this.form.submit()">
-                                <option value="Pending" <?php echo (isset($_GET['status']) && $_GET['status'] == 'Pending') ? 'selected' : ''; ?>>Pending</option>
-                                <option value="Cancelled" <?php echo (isset($_GET['status']) && $_GET['status'] == 'Cancelled') ? 'selected' : ''; ?>>Cancelled</option>
-                                <option value="Accepted" <?php echo (isset($_GET['status']) && $_GET['status'] == 'Accepted') ? 'selected' : ''; ?>>Accepted</option>
-                            </select>
-                        </form>
-                    </div>
-              
-
-                </div>
-               
-
-                <div class="appointment-req-table">
-                    <?php
-                    // Check for connection 
-                    if ($conn) {
-                        // Default status filter value
-                        $statusFilter = 'Pending';
-
-                        // Check if a specific status is selected in the dropdown
-                        if (isset($_GET['status']) && in_array($_GET['status'], ['Pending', 'Cancelled', 'Accepted'])) {
-                            $statusFilter = $_GET['status'];
-                        }
-
-                        $query = "SELECT * FROM appointments WHERE status = '$statusFilter'";
-                        $result = mysqli_query($conn, $query);
-
-                        if ($result) { ?>
-                        <div class="table-div">
-                            <table class="table">
-                                <thead class="thead">
-                                    <tr>
-                                        <th class="fixed" scope="col">Name</th>
-                                        <th class="fixed" scope="col">Date</th>
-                                        <th class="fixed" scope="col">Time</th>
-                                        <th class="fixed" scope="col">Service</th>
-                                        <th class="fixed" scope="col">Status</th>
-                                        <th class="fixed" scope="col">Screenshot</th>
-                                        <th class="fixed" scope="col">Actions</th>
-                                    </tr>
-                                </thead>
-
-                                <tbody> <?php ;
-
-
-
-                            while ($row = mysqli_fetch_assoc($result)) {
-                                echo '<tr>
-                                    <td>' . $row['first_name'] . ' ' . $row['last_name'] . '</td>
-                                    <td>' . date('F j, Y', strtotime($row['date'])) . '</td>
-                                    <td>' . date('h:i A', strtotime($row['time'])) . '</td>
-                                    <td>' . $row['service'] . '</td>
-                                    <td>' . $row['status'] . '</td>
-                                    <td>' . $row['screenshot'] . '</td>
-                                    ' . (($row['status'] == 'Pending') ? '<td class="action">
-                                        <button class="button" id="cancel"  onclick="openCancelModal(' . $row['id'] . ')">Cancel</button>
-                                        <button class="button" id="reschedule" onclick="openReschedModal(' . $row['id'] . ')">Reschedule</button>
-                                        <button class="button" id="accept" onclick="openAcceptModal(' . $row['id'] . ')">Accept</button>
-                                    </td>' : '') . '
-                                </tr>';
+                            // Check if a specific status is selected in the dropdown
+                            if (isset($_GET['status']) && in_array($_GET['status'], ['Pending', 'Cancelled', 'Accepted'])) {
+                                $statusFilter = $_GET['status'];
                             }
 
-                            echo '</tbody></table>';
-                        } else {
-                            echo 'Error: ' . mysqli_error($conn);
-                        }
-                    } else {
-                        echo 'Database connection failed: ' . mysqli_connect_error();
-                    }
-                    // close connections
-                    // mysqli_close($conn);
-                ?>
+                            $query = "SELECT * FROM appointments WHERE status = '$statusFilter'";
+                            $result = mysqli_query($conn, $query);
 
-            </div>
-        </section>
-    </div>
+                            if ($result) { ?>
+                            <div class="table-div">
+                                <table class="table">
+                                    <thead class="thead">
+                                        <tr>
+                                            <th class="fixed" scope="col">Name</th>
+                                            <th class="fixed" scope="col">Date</th>
+                                            <th class="fixed" scope="col">Time</th>
+                                            <th class="fixed" scope="col">Service</th>
+                                            <th class="fixed" scope="col">Status</th>
+                                            <th class="fixed" scope="col">Screenshot</th>
+                                            <th class="fixed" scope="col">Actions</th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody> <?php ;
+
+
+
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                    echo '<tr>
+                                        <td>' . $row['first_name'] . ' ' . $row['last_name'] . '</td>
+                                        <td>' . date('F j, Y', strtotime($row['date'])) . '</td>
+                                        <td>' . date('h:i A', strtotime($row['time'])) . '</td>
+                                        <td>' . $row['service'] . '</td>
+                                        <td>' . $row['status'] . '</td>
+                                        <td>' . $row['screenshot'] . '</td>
+                                        ' . (($row['status'] == 'Pending') ? '<td class="action">
+                                            <button class="button" id="cancel"  onclick="openCancelModal(' . $row['id'] . ')">Cancel</button>
+                                            <button class="button" id="reschedule" onclick="openReschedModal(' . $row['id'] . ')">Reschedule</button>
+                                            <button class="button" id="accept" onclick="openAcceptModal(' . $row['id'] . ')">Accept</button>
+                                        </td>' : '') . '
+                                    </tr>';
+                                }
+
+                                echo '</tbody></table>';
+                            } else {
+                                echo 'Error: ' . mysqli_error($conn);
+                            }
+                        } else {
+                            echo 'Database connection failed: ' . mysqli_connect_error();
+                        }
+                        // close connections
+                        // mysqli_close($conn);
+                    ?>
+
+                </div>
+            </section>
+        </div>
+    </section>
+
     <?php require_once('modal/accept-request.php');?>
     <?php require_once('modal/resched-appointment.php');?>
     <?php require_once('modal/cancel-appointment.php');?>
