0a3ad7e3a5e83e1aba930edef93f0a18d24877e3
EdisonP <133362311+edions@users.noreply.github.com>
2023-12-27 23:19:17 +0800

    Refactor: admin-patRecHistory

--- admin-patRecHistory.php
+++ admin-patRecHistory.php
@@ -144,97 +144,98 @@
 <body>
     <?php include('admin-sidebar.php');?> 
 
-    <div class="patRecHistory-container">
-        <div class="container">
-            <h1>Patient Records and History</h1>
+    <section class="dashboard" id="dashboard">
+        <div class="patRecHistory-container">
+            <div class="container">
+                <h1>Patient Records and History</h1>
 
-            <div class="row-container">
-                <!-- <div class="row one">
-                    <h3><b>Search Patient:</b></h3>
-                </div> -->
-
-                <div class="row two">
-                    <!-- get user input -->
-                    <!-- <div class="input-box">
-                        <input type="text" id="getName" placeholder="Search patient">
+                <div class="row-container">
+                    <!-- <div class="row one">
+                        <h3><b>Search Patient:</b></h3>
                     </div> -->
 
-                    <div class="input-box search-container">
-                        <i class="fa fa-search icon"></i>
-                        <input type="text" id="getName" placeholder="Search patient">
+                    <div class="row two">
+                        <!-- get user input -->
+                        <!-- <div class="input-box">
+                            <input type="text" id="getName" placeholder="Search patient">
+                        </div> -->
+
+                        <div class="input-box search-container">
+                            <i class="fa fa-search icon"></i>
+                            <input type="text" id="getName" placeholder="Search patient">
+                        </div>
+
                     </div>
-
                 </div>
-            </div>
-           
-            <!-- display patients_table1's data that are from verified accounts -->
-            <table class="table table-striped">
-                <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th colspan="3">Actions</th>
-                </tr>
-                </thead>
             
-                <tbody id="showdata">
-                <?php  
+                <!-- display patients_table1's data that are from verified accounts -->
+                <table class="table table-striped">
+                    <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th colspan="3">Actions</th>
+                    </tr>
+                    </thead>
+                
+                    <tbody id="showdata">
+                    <?php  
 
-                        // SELECT * FROM Customers ORDER BY Country DESC;
-                        $sql = "SELECT * FROM patients_table1 WHERE verified = 1 ORDER BY ID DESC";
-                        $query = mysqli_query($conn,$sql);
+                            // SELECT * FROM Customers ORDER BY Country DESC;
+                            $sql = "SELECT * FROM patients_table1 WHERE verified = 1 ORDER BY ID DESC";
+                            $query = mysqli_query($conn,$sql);
 
-                        while($row = mysqli_fetch_assoc($query))
-                        {
-                        $email = $row['active_gmail'];
-                        $first_name = $row['first_name'];
-                        $last_name = $row['last_name'];
-                        $patient_id = $row['id'];
+                            while($row = mysqli_fetch_assoc($query))
+                            {
+                            $email = $row['active_gmail'];
+                            $first_name = $row['first_name'];
+                            $last_name = $row['last_name'];
+                            $patient_id = $row['id'];
 
-                        echo"<tr>";
-                            echo"<td><h6>".$row['id']."</h6></td>";
-                            echo"<td> <h6>".$row['first_name']. ' '. $row['last_name']."</h6> </td>";
-                            echo"<td>".$row['active_gmail']."</td>";
-                            // echo "<td><a class='anchor3' href='p_basic-info.php?active_gmail=$email&first_name=$first_name&last_name=$last_name'>Basic Info</a></td>";
-                            // echo "<td><a class='anchor1' href='more-info.php?active_gmail=$email&first_name=$first_name&last_name=$last_name'>Medical Info</a></td>";
-                            // echo "<td><a class='anchor2' href='p_booking-history.php?active_gmail=$email&first_name=$first_name&last_name=$last_name'>Booking History</a></td>";
-                            // echo "<td> <button class='button patientInfo' id='viewInfo' onclick='openviewPatientInfoModal(" . $row['id'] . ")'>Basic Info</button> </td>";
-                            // echo "<td> <button class='button patientMedical' id='viewMedical' onclick='openviewMedicalInfoModal(" . $row['id'] . ")'>Medical Info</button> </td>";
-                            // echo "<td> <button class='button patientBooking' id='viewBooking' onclick='openviewBookingHistoryModal(" . $row['id'] . ")'>Booking History</button> </td>";
-                            echo "<td><a class='anchor3' href='p_treatment-records.php?id=$patient_id&first_name=$first_name&last_name=$last_name'>Treatment Records</a></td>";
-                            echo "<td><a class='anchor2' href='p_booking-history.php?active_gmail=$email&first_name=$first_name&last_name=$last_name'>Booking History</a></td>";
+                            echo"<tr>";
+                                echo"<td><h6>".$row['id']."</h6></td>";
+                                echo"<td> <h6>".$row['first_name']. ' '. $row['last_name']."</h6> </td>";
+                                echo"<td>".$row['active_gmail']."</td>";
+                                // echo "<td><a class='anchor3' href='p_basic-info.php?active_gmail=$email&first_name=$first_name&last_name=$last_name'>Basic Info</a></td>";
+                                // echo "<td><a class='anchor1' href='more-info.php?active_gmail=$email&first_name=$first_name&last_name=$last_name'>Medical Info</a></td>";
+                                // echo "<td><a class='anchor2' href='p_booking-history.php?active_gmail=$email&first_name=$first_name&last_name=$last_name'>Booking History</a></td>";
+                                // echo "<td> <button class='button patientInfo' id='viewInfo' onclick='openviewPatientInfoModal(" . $row['id'] . ")'>Basic Info</button> </td>";
+                                // echo "<td> <button class='button patientMedical' id='viewMedical' onclick='openviewMedicalInfoModal(" . $row['id'] . ")'>Medical Info</button> </td>";
+                                // echo "<td> <button class='button patientBooking' id='viewBooking' onclick='openviewBookingHistoryModal(" . $row['id'] . ")'>Booking History</button> </td>";
+                                echo "<td><a class='anchor3' href='p_treatment-records.php?id=$patient_id&first_name=$first_name&last_name=$last_name'>Treatment Records</a></td>";
+                                echo "<td><a class='anchor2' href='p_booking-history.php?active_gmail=$email&first_name=$first_name&last_name=$last_name'>Booking History</a></td>";
 
-                            echo"</tr>";   
-                        }
-                    ?>
-                </tbody>
-            </table>
+                                echo"</tr>";   
+                            }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+
+            <!-- include the modal files -->
+            <?php require_once('modal/view-patientInfo.php');?>
+            <?php require_once('modal/view-patientMedical.php');?>
+
         </div>
-
-        <!-- include the modal files -->
-        <?php require_once('modal/view-patientInfo.php');?>
-        <?php require_once('modal/view-patientMedical.php');?>
-
-
-        <!-- connected to searchajax.php -->
-        <script>
-            $(document).ready(function(){
-            $('#getName').on("keyup", function(){
-                var getName = $(this).val();
-                $.ajax({
-                method:'POST',
-                url:'searchajax.php',
-                data:{name:getName},
-                success:function(response)
-                {
-                        $("#showdata").html(response);
-                } 
-                });
+    </section>
+    
+<!-- connected to searchajax.php -->
+    <script>
+        $(document).ready(function(){
+        $('#getName').on("keyup", function(){
+            var getName = $(this).val();
+            $.ajax({
+            method:'POST',
+            url:'searchajax.php',
+            data:{name:getName},
+            success:function(response) {
+                    $("#showdata").html(response);
+            } 
             });
-            });
-        </script>
-    </div>
+        });
+        });
+    </script>
 
 </body>
 </html>
